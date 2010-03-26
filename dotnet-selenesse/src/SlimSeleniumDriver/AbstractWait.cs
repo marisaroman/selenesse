85a1467da322371fea6b7e0c0f68ef0b15b2f24e
dckismet <dcannan@gmail.com>
2010-03-26 10:32:49 -0400

    Adding dotnet folder. Includes src file with full solution and project files and fitnesse structure with dotnet pages added
    Signed-off-by: dckismet <dcannan@gmail.com>

---
+++ dotnet-selenesse/src/SlimSeleniumDriver/AbstractWait.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Selenesse
+{
+    abstract class AbstractWait
+    {
+        protected int pollingFrequency = 100;
+        public void Wait(String message, int timeoutMillis)
+        {
+            DateTime start = DateTime.Now;
+            while (timeoutMillis > DateTime.Now.Subtract(start).TotalMilliseconds)
+            {
+                if (Until()) return;
+                System.Threading.Thread.Sleep(pollingFrequency);
+            }
+            throw new ApplicationException(message);
+        }
+        public abstract bool Until();
+    }
+}
---
+++ dotnet-selenesse/src/SlimSeleniumDriver/Waiters.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Selenium;
+
+namespace Selenesse
+{
+
+    /**************************************************/
+    /* waiter subclasses */
+    class WaitForTextToAppear : AbstractWait
+    {
+        private String text;
+        private ISelenium instance;
+
+        public WaitForTextToAppear(String text, ISelenium selenium)
+        {
+            this.text = text;
+            this.instance = selenium;
+        }
+        public override bool Until()
+        {
+            return instance.IsTextPresent(text);
+        }
+    }
+    class WaitForChecked : AbstractWait
+    {
+        private String locator;
+        private bool ischecked;
+        private ISelenium instance;
+        public WaitForChecked(String locator, bool ischecked, ISelenium selenium)
+        {
+            this.locator = locator;
+            this.ischecked = ischecked;
+            this.instance = selenium;
+        }
+        public override bool Until()
+        {
+            if (!instance.IsElementPresent(locator)) return false;
+            return instance.IsChecked(locator) == ischecked;
+        }
+    }
+    class WaitForElementToAppear : AbstractWait
+    {
+        private String text;
+        private ISelenium instance;
+        public WaitForElementToAppear(String text, ISelenium selenium)
+        {
+            this.text = text;
+            this.instance = selenium;
+        }
+        public override bool Until()
+        {
+            return instance.IsElementPresent(text);
+        }
+    }
+    class WaitForElementToDisappear : AbstractWait
+    {
+        private String text;
+        private ISelenium instance;
+        public WaitForElementToDisappear(String text, ISelenium selenium)
+        {
+            this.text = text;
+            this.instance = selenium;
+        }
+        public override bool Until()
+        {
+            return (!instance.IsElementPresent(text));
+        }
+    }
+
+    class WaitForFieldValue : AbstractWait
+    {
+        private String text;
+        private String elementLocator;
+        private ISelenium instance;
+        public WaitForFieldValue(String element, String value, ISelenium selenium)
+        {
+            this.text = value;
+            this.elementLocator = element;
+            this.instance = selenium;
+        }
+        public override bool Until()
+        {
+            String value = instance.GetValue(elementLocator);
+            if (value == null) return false;
+            return value.IndexOf(text) >= 0;
+        }
+    }
+    /**************************************************/
+}
